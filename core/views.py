f91405a18ea8bb24894bf81abb8299b26b5fda02
Xander <denzelwashington913@gmail.com>
2024-10-23 12:16:18 +0100

    removed super user

--- core/views.py
+++ core/views.py
@@ -66,17 +66,7 @@ def _provider_data(provider:Provider):
     }
 
 def index(request):
-    admin = User.objects.create_superuser(username='BMI_ADMIN',email='contact@cbdmi.com',password='GgfB&f&*Fe4S6XmGwCefU@N&')
-    company = Provider.objects.create(
-        name = 'BMI',
-        label = 'company',
-        phone_number = '',
-        city = 'Yaoundé',
-        product_type = 'Varié',
-        user = admin,
-    )
-    admin.save()
-    company.save()
+    
     return render(request,"core/index.html")
 
 def products(request):
