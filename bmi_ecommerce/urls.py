0cdfe5cd6c5801fa464dc7b08fbb343f7b2731c3
Ngaikam Alex <alexngaikama913@gmail.com>
2025-02-24 09:58:57 +0100

    email login api config added

--- bmi_ecommerce/urls.py
+++ bmi_ecommerce/urls.py
@@ -22,9 +22,36 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 from rest_framework_simplejwt.views import TokenObtainPairView,TokenRefreshView
-from customJwt import customTokenObtainPairView
+#from customJwt import customTokenObtainPairView
+#import customJwt
 
 
+#OBTAINING TOKENS LOGIN with EMAIL
+from django.contrib.auth.models import User
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework import serializers
+
+
+class CustomTokenPairSerializer(TokenObtainPairSerializer):
+
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        token['email'] = user.email
+        return token
+    
+
+    def validate(self, attrs):
+
+        try:
+            user = User.objects.get(email = attrs['email'])
+        except User.DoesNotExist:
+            raise serializers.ValidationError('Invalid email or password')    
+
+        return super().validate(attrs)
+
+class customTokenObtainPairView(TokenObtainPairView):
+    serializer_class = CustomTokenPairSerializer
 
 
 urlpatterns = [
