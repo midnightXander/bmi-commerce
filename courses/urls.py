341cf1ffad4ef16fff602ebb9e78b1eb7142edad
Ngaikam Alex <alexngaikama913@gmail.com>
2025-07-02 16:30:25 +0100

    added courses

--- courses/urls.py
+++ courses/urls.py
@@ -4,5 +4,8 @@ from . import views
 app_name = 'courses'
 
 urlpatterns = [
-    path('languages',views.index, name='index'),
+    path('',views.index, name='index'),
+    path('tests',views.tests, name='tests'),
+    path('<str:test_type>/<str:module>',views.module, name='module'),
+    path('tests/courses/<str:slug>',views.course, name='course'),
 ] 
--- courses/views.py
+++ courses/views.py
@@ -3,3 +3,18 @@ from django.shortcuts import render
 
 def index(request):
     return render(request,"courses/index.html")
+
+def tests(request):
+    return render(request,"courses/tests.html")
+
+def courses(request):
+    return render(request,"courses/courses.html")
+
+def module(request, test_type, module):
+    return render(request,"courses/module.html",{
+        'test': test_type,
+        'module' : module
+    })
+
+def course(request, slug):
+    return render(request,"courses/course.html")
